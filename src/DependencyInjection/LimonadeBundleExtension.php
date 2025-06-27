54d4ac7e4860a87c36e1846b7590b9dbd64bf2f2
Alexandre DELAISTRE <a.quilan-delaistre@voxagrume.fr>
2025-06-27 16:46:28 +0200

    add service autowiring

--- src/DependencyInjection/LimonadeBundleExtension.php
+++ src/DependencyInjection/LimonadeBundleExtension.php
@@ -1,13 +1,20 @@
 <?php
 namespace Agrume\Limonade\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class LimonadeBundleExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
         // Ici tu peux charger ta configuration et services
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../../../Resources/config')
+        );
+        $loader->load('services.yaml');
     }
 }
